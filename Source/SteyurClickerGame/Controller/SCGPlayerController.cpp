91025ff7a4e74e7cf09554353427cec133e9b341
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2026-05-28 23:44:05 +0300

    Moving everything made so far to C++ WIP

--- Source/SteyurClickerGame/Controller/SCGPlayerController.cpp
+++ Source/SteyurClickerGame/Controller/SCGPlayerController.cpp
@@ -2,4 +2,39 @@
 
 
 #include "Controller/SCGPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Character/SCGPlayerCharacter.h"
+#include "EnhancedInputComponent.h"
 
+void ASCGPlayerController::OnPossess(APawn* aPawn)
+{
+    Super::OnPossess(aPawn);
+
+    SetupEnhancedInput();
+    SetupInputActions(aPawn);
+}
+
+void ASCGPlayerController::SetupEnhancedInput()
+{
+    if (!IsLocalController() || !DefaultMappingContext) return;
+
+    ULocalPlayer* const LocalPlayer = GetLocalPlayer();
+    if (!LocalPlayer) return;
+
+    UEnhancedInputLocalPlayerSubsystem* const InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+    if (!InputSubsystem) return;
+
+    InputSubsystem->ClearAllMappings();
+    InputSubsystem->AddMappingContext(DefaultMappingContext, InputPriority);
+}
+
+void ASCGPlayerController::SetupInputActions(APawn* const ControlledPawn)
+{
+    ASCGPlayerCharacter* const PlayerCharacter = Cast<ASCGPlayerCharacter>(ControlledPawn);
+    if (!PlayerCharacter) return;
+
+    UEnhancedInputComponent* const EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+    if (!EnhancedInputComponent) return;
+
+    EnhancedInputComponent->bind
+}
--- Source/SteyurClickerGame/Controller/SCGPlayerController.h
+++ Source/SteyurClickerGame/Controller/SCGPlayerController.h
@@ -14,4 +14,19 @@ class STEYURCLICKERGAME_API ASCGPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+protected:
+    virtual void OnPossess(APawn* aPawn) override;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+    UInputMappingContext* DefaultMappingContext;
+
+
+private:
+
+    /* Sets up enhanced input ONCE on posession of character */
+    void SetupEnhancedInput();
+
+    void SetupInputActions(APawn* const ControlledPawn);
 };
